390a68cebc7c11e0a2f9bf2b86e7007f8cf6dc3b
Zheng Chen <chenzheng0326@gmail.com>
2019-02-04 08:50:42 +1100

    Add encode and decode password function - final version

--- com-exercise-project/src/com/exercise/utilities/LoginFactory.java
+++ com-exercise-project/src/com/exercise/utilities/LoginFactory.java
@@ -1,5 +1,6 @@
 package com.exercise.utilities;
 
+import org.apache.commons.codec.binary.Base64;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -12,15 +13,22 @@ public class LoginFactory {
 	public void login() throws InterruptedException {
 		driver.get("https://excerise.atlassian.net/wiki/");
 		String username = "chenzheng0326@gmail.com";
-		String password = "m4a1Zheng0326";
+		String password = decodePassword("IXpoZW5nY2hlbjMyNkV4ZXJjaXNl");
 
 		LoginPageFactory loginPage = new LoginPageFactory(driver);
 		loginPage.logIn(username, password);
 	}
 
-	// constructor
+	//decode encoded password
+	public static String decodePassword(String password) {
+		byte[] decodedPassword = Base64.decodeBase64(password);
+		return new String(decodedPassword);
+	}
+	
+	//constructor
 	public LoginFactory(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+
 }
